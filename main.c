c368ab2a10f6f8adadc853a17e1789bcc7784171
Suiciniv01 <viniciusfranca567@gmail.com>
2024-11-17 14:09:18 -0300

    Agora vai.

--- main.c
+++ main.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void imprime_histograma(int* vendas[], int num_categorias) {
+void imprime_histograma(int* vendas, int num_categorias) {
     for (int i = 0; i < num_categorias; i++) {
-        printf("%d ", *(*(vendas + i)));
+        printf("%d ", vendas[i]); 
 
-        if (*(*(vendas + i)) > 0) { 
-            for (int j = 0; j < *(*(vendas + i)); j++) {
+        if (vendas[i] > 0) { 
+            for (int j = 0; j < vendas[i]; j++) {
                 printf("*");
             }
         }
-        printf("\n"); 
-
+        printf("\n");
     }
 }
 
@@ -24,26 +23,17 @@ int main() {
         return 1;
     }
 
-    int** vendas = (int**)malloc(num_categorias * sizeof(int*));
+    int* vendas = (int*)malloc(num_categorias * sizeof(int));
     if (vendas == NULL) {
         return 1;
     }
 
     for (int i = 0; i < num_categorias; i++) {
-        *(vendas + i) = (int*)malloc(sizeof(int));
-        if (*(vendas + i) == NULL) {
-            return 1;
-        }
-
-        scanf("%d", *(vendas + i)); 
-
+        scanf("%d", &vendas[i]); 
     }
 
     imprime_histograma(vendas, num_categorias);
 
-    for (int i = 0; i < num_categorias; i++) {
-        free(*(vendas + i));
-    }
     free(vendas);
 
     return 0;
