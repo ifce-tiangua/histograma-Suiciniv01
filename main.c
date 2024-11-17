ad3a62d28a983f0163f3c0665ca74882ddcda811
Suiciniv01 <viniciusfranca567@gmail.com>
2024-11-17 13:35:28 -0300

    Atvidade "Histograma" concluido. Aluno: Marcos Vinicius Costa de França.

--- main.c
+++ main.c
@@ -1,10 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void imprime_histograma(int* vendas[], int num_categorias) {
+
+    for (int i = 0; i < num_categorias; i++) {
+        printf("%d ", *vendas[i]); 
+
+        if (*vendas[i] > 0) {
+            for (int j = 0; j < *vendas[i]; j++) {
+                printf("*"); 
+            }
+        }
+
+        printf("\n"); 
+    }
+    
+}
+
 int main(){
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+    int num_categorias, i;
+    scanf("%d", &num_categorias);
     
+    int** vendas = (int**) malloc(num_categorias * sizeof(int*));
+
+    for (i = 0; i < num_categorias; i++){
+        vendas[i] = (int*) malloc(sizeof(int));
+        scanf("%d", vendas[i]);
+
+    }
+
+    imprime_histograma(vendas, num_categorias);
+
+    for (int i = 0; i < num_categorias; i++) {
+        free(vendas[i]);
+    }
+    free(vendas);
+ 
     return 0;
 }
+
+
