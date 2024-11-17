0eb6f271ae8b97c18c4f91f5f2fc8dc6b8e255b5
Suiciniv01 <viniciusfranca567@gmail.com>
2024-11-17 13:41:17 -0300

    Atv "Histograma" concluido. Aluno: Marcos Vinicius Costa de Franca.

--- main.c
+++ main.c
@@ -4,17 +4,17 @@
 void imprime_histograma(int* vendas[], int num_categorias) {
 
     for (int i = 0; i < num_categorias; i++) {
-        printf("%d ", *vendas[i]); 
+        printf("%d ", *(*(vendas+ i))); 
 
         if (*vendas[i] > 0) {
-            for (int j = 0; j < *vendas[i]; j++) {
+            for (int j = 0; j < *(*(vendas+ i)); j++) {
                 printf("*"); 
             }
         }
 
         printf("\n"); 
     }
-    
+
 }
 
 int main(){
@@ -25,15 +25,15 @@ int main(){
     int** vendas = (int**) malloc(num_categorias * sizeof(int*));
 
     for (i = 0; i < num_categorias; i++){
-        vendas[i] = (int*) malloc(sizeof(int));
-        scanf("%d", vendas[i]);
+        *(vendas+ i) = (int*) malloc(sizeof(int));
+        scanf("%d", *(vendas+ i));
 
     }
 
     imprime_histograma(vendas, num_categorias);
 
     for (int i = 0; i < num_categorias; i++) {
-        free(vendas[i]);
+        free(*(vendas + i));
     }
     free(vendas);
  
