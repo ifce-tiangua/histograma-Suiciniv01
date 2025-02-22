6c7696b8e6785bed9a9e4a739d3823a3eb286c4f
Marcos Vinicius França <108764784+Suiciniv01@users.noreply.github.com>
2025-02-22 01:47:59 +0000

    test

--- main.c
+++ main.c
@@ -1,40 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void imprime_histograma(int *vendas, int num_categorias) {
-    int i, j;
+void imprime_histograma(int* vendas[], int num_categorias) {
+    int vazio = 1; 
 
-    for (i = 0; i < num_categorias; i++) {
-        printf("%d ", vendas[i]); 
-
-        for (j = 0; j < vendas[i]; j++) {
-            printf("*");
+    // Verifica se há vendas
+    for (int i = 0; i < num_categorias; i++) {
+        if (*vendas[i] > 0) {
+            vazio = 0; 
+            break;  
         }
-        
-        printf("\n");
+    }
 
+
+    if (vazio) {
+        printf("vazio\n");
+        return;
+    }
+
+    // Caso contrário, imprime o histograma
+    for (int i = 0; i < num_categorias; i++) {
+        printf("%d ", *vendas[i]); 
+        for (int j = 0; j < *vendas[i]; j++) {
+            printf("*"); 
+        }
+        printf("\n");
     }
 }
 
 int main() {
-    int num_categorias, i, *vendas;
+    int num_categorias;
 
     scanf("%d", &num_categorias);
 
-    if (num_categorias == 0){
-        printf("vazio\n");
+    int** vendas = (int**)malloc(num_categorias * sizeof(int*));
+
+    if (vendas == NULL) {
+        printf("Erro na alocação de memória.\n");
+        return 1;
     }
-    
 
-    vendas = (int*) malloc(num_categorias * sizeof(int));
+    for (int i = 0; i < num_categorias; i++) {
+        vendas[i] = (int*)malloc(sizeof(int));  
+        if (vendas[i] == NULL) {
+            printf("Erro na alocação de memória para a categoria %d.\n", i + 1);
+            return 1;
+        }
 
-    for (i = 0; i < num_categorias; i++) {
-        scanf("%d", &vendas[i]);
+        scanf("%d", vendas[i]);
     }
 
     imprime_histograma(vendas, num_categorias);
 
-    free(vendas);
-    
+    for (int i = 0; i < num_categorias; i++) {
+        free(vendas[i]);  
+    }
+    free(vendas);  
+
     return 0;
 }
